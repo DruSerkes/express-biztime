a1b00bb6d72ae19b68f71e5c3df68190db7b95cd
Dru Serkes <46517695+DruSerkes@users.noreply.github.com>
2020-07-15 14:42:33 -0400

    Delete practice.sql

--- practice.sql
+++
@@ -1,6 +0,0 @@
-SELECT c.code, c.name, c.description, t.tag
-FROM messages AS m
-    LEFT JOIN messages_tags AS mt
-    ON m.id = mt.message_id
-    LEFT JOIN tags AS t ON mt.tag_code = t.code
-WHERE m.id = $1
